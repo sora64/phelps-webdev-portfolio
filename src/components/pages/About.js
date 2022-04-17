3061685409781ffd49fc66298163a8176e5b718f
Alan Phelps <phelpsa64@gmail.com>
2022-04-17 13:46:26 -0400

    Contact component completed and implemented

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,10 +3,18 @@ import React from "react";
 export default function About() {
   return (
     <section id="about-me">
-            <article className="content">
-                <h2>About Me</h2>
-                <p>My name is Alan Phelps, and I am currently in the process of becoming a full-stack web-developer through the University of Richmond's Coding Bootcamp program. I have many years of working experience, from selling books to making coffee, from facilitating a high ropes course to teaching Latin. I'm resourceful, inquisitive, and efficient. If you are a good employer looking for a viable candidate for a web-development position, I'm your guy!</p>
-            </article>
-        </section>
+      <article className="content">
+        <h2>About Me</h2>
+        <p>
+          My name is Alan Phelps, and I am currently in the process of becoming
+          a full-stack web-developer through the University of Richmond's Coding
+          Bootcamp program. I have many years of working experience, from
+          selling books to making coffee, from facilitating a high ropes course
+          to teaching Latin. I'm resourceful, inquisitive, and efficient. If you
+          are a good employer looking for a viable candidate for a
+          web-development position, I'm your guy!
+        </p>
+      </article>
+    </section>
   );
 }
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact-me">
+      <article className="content">
+        <h2>Contact Me</h2>
+        <article className="contact-address">
+          <h3>Alan Phelps</h3>
+          <br />
+          <p className="addresses">Mail: P.O. Box 4496, Richmond, VA
+          </p>
+          <br />
+          <p className="addresses">
+            Phone : (252) 796-7552
+            <br />
+          </p>
+          <br />
+          <p className="addresses">
+            Email: <a href="mailto:phelpsa64@gmail.com">phelpsa64@gmail.com</a>
+          </p>
+          <br />
+          <p className="addresses">
+            GitHub: <a href="https://github.com/sora64">github.com/sora64</a>
+          </p>
+          <br />
+          <p className="addresses">
+            <a href="https://www.linkedin.com/in/alan-phelps-43ab57b6">
+              LinkedIn Profile
+            </a>
+          </p>
+        </article>
+      </article>
+    </section>
+  );
+}
