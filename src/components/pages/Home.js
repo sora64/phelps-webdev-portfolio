8f6c6582bc4f4c57020eedcb96638e88928dbeb5
Alan Phelps <phelpsa64@gmail.com>
2022-04-17 11:50:50 -0400

    added elements to Home component

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="hero-welcome">
+      <h2>Welcome!</h2>
+      <div className="hero-content">
+        <img
+          src="./Assets/Images/IMG_0422.jpeg"
+          alt="Alan smiling in front of the Eiffel Tower in Paris, France."
+        />
+        <p>
+          Welcome to my portfolio! Read about me, see my work, and contact me
+          for any and all inquiries. Thank you, and have a great day!
+        </p>
+      </div>
+    </div>
+  );
+}
