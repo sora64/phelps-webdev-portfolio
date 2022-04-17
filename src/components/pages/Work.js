e3d621a72117d1875142ac6d956f8fe407293b2a
Alan Phelps <phelpsa64@gmail.com>
2022-04-17 13:34:13 -0400

    Work component finished and implemented

---
+++ src/components/pages/Work.js
@@ -0,0 +1,129 @@
+import React from 'react';
+
+export default function Work() {
+  return (
+    <section id="work" className="section-alt">
+            <article className="content">
+                <h2 className="text-right">Work</h2>
+                <div className="projects">
+                    <a href="https://github.com/sora64/photo-port">
+                        <img src="images/photoPortPicjpg.jpeg" alt="PhotoPort GitHub repo link" width="500" height="500" />
+                        <div className="link-text lt1">Photo-Port</div>
+                    </a>
+                    <a href="https://github.com/sora64/track-that-budget-baby">
+                        <img src="images/budgetTrackerScreenshot.png" alt="Budget Tracker GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt2">Track That Budget!</div>
+                    </a>
+                    <a href="https://github.com/sora64/food-festival">
+                        <img src="images/foodFestival.jpg" alt="Food Festival GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt2">Food Festival</div>
+                    </a>
+                    <a href="https://github.com/sora64/pizza-hunt">
+                        <img src="images/pizzaHuntScreenshot.png" alt="Pizza-Hunt GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Pizza-Hunt</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-social-media-api">
+                        <img src="images/socialMediaApiScreenshot.png" alt="Social Media API GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Social-Media-API</div>
+                    </a>
+                    <a href="https://gist.github.com/sora64/b448058c9b5ecf07ee0e60bf59c3f8f8">
+                        <img src="images/dateTimeRegexGistScreenshot.png" alt="DateTime Regex GitHub Gist link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">DateTime Regex Gist</div>
+                    </a>
+                    <a href="https://github.com/sora64/sk8spotter">
+                        <img src="images/Sk8SpotterScreenshot.png" alt="Sk8Spotter GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Sk8Spotter</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-tech-blog">
+                        <img src="images/phelpsTechBlogScreenshot.png" alt="u-develop-it GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">phelps-tech-blog</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-eCommerce-backend-challenge/">
+                        <img src="images/eCommerceBackendScreenshot.png" alt="u-develop-it GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">E-Commerce Backend</div>
+                    </a>
+                    <a href="https://github.com/sora64/just-tech-news">
+                        <img src="images/justTechNewsPlaceholderImage.jpeg" alt="u-develop-it GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">just-tech-news</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-employee-tracker-challenge">
+                        <img src="images/employeeTrackerScreenshot.png" alt="u-develop-it GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Employee Tracker</div>
+                    </a>
+                    <a href="https://github.com/sora64/u-develop-it">
+                        <img src="images/uDevelopItScreenshot.png" alt="u-develop-it GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">u-develop-it</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-note-taker-challenge">
+                        <img src="images/noteTakerScreenshot.png" alt="Note Taker GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Note Taker</div>
+                    </a>
+                    <a href="https://github.com/sora64/zookeepr">
+                        <img src="images/zookeeprScreenshot.png" alt="Zoo Keepr GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Zoo Keepr</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-team-profile-generator-challenge">
+                        <img src="images/teamProfileGeneratorScreenshot.png" alt="Team Profile Generator GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Team Profile Generator</div>
+                    </a>
+                    <a href="https://github.com/sora64/jest-another-RPG">
+                        <img src="images/jestAnotherRPGplaceholderImage.jpeg" alt="Jest Another RPG Generator GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Jest Another RPG</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-README-generator-challenge">
+                        <img src="images/readmeGeneratorScreenshot.png" alt="README Generator GitHub repo lik" width="400" height="300" />
+                        <div className="link-text lt3AndUp">README Generator</div>
+                    </a>
+                    <a href="https://github.com/sora64/portfolio-generator">
+                        <img src="images/portfolioGeneratorScreenshot.png" alt="Portfolio Geneartor GitHub repo lik" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Portfolio Generator</div>
+                    </a>
+                    <a href="https://github.com/sora64/Music-You-May-Like">
+                        <img src="images/musicYouMayLikeScreenshot.png" alt="Music You May Like GitHub repo lik" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Music You May Like</div>
+                    </a>
+                    <a href="https://github.com/sora64/phelps-weather-dashboard-challenge">
+                        <img src="images/weatherDashboardScreenshot.png" alt="Weather Dashboard GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Weather Dashboard</div>
+                    </a>
+                    <a href="https://github.com/sora64/git-it-done">
+                        <img src="images/gitItDoneScreenshot.png" alt="Git-It-Done GitHub repository link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Git-It-Done</div>
+                    </a>
+                    <a href="https://github.com/sora64/workday-scheduler">
+                        <img src="images/WorkDaySchedulerScreenshot.png" alt="Workday Scheduler GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Workday Scheduler</div>
+                    </a>
+                    <a href="https://github.com/sora64/taskmaster-pro">
+                        <img src="images/taskMasterProScreenShot.png" alt="Taskmaster Pro GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Taskmaster Pro</div>
+                    </a>
+                    <a href="https://github.com/sora64/code-quiz-challenge">
+                        <img src="images/Coding Quiz Challenge pic.png" alt="Coding Quiz Challenge GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Coding Quiz Challenge</div>
+                    </a>
+                    <a href="https://sora64.github.io/taskinator/">
+                        <img src="images/Taskinator Webpabe.png" alt="Taskinator GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Taskinator</div>
+                    </a>
+                    <a href="https://github.com/sora64/password-generator-challenge">
+                        <img src="images/passWordGenerator.png" alt="Password Generator GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Password Generator</div>
+                    </a>
+                    <a href="https://github.com/sora64/robot-gladiators">
+                        <img src="images/robot-face.png" alt="Robot Gladiators GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Robot Gladiators</div>
+                    </a>
+                    <a href="https://github.com/sora64/Horiseon-Code-Refactor">
+                        <img src="images/horiseon-marketing.jpg" alt="Horiseon Code Refactor GitHub repo link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Horiseon Code Refactor</div>
+                    </a>
+                    <a href="https://github.com/sora64/run-buddy">
+                        <img src="images/hero-bg.jpg" alt="Run-Buddy website link" width="400" height="300" />
+                        <div className="link-text lt3AndUp">Run-Buddy</div>
+                    </a>
+                </div>
+            </article>
+        </section>
+  );
+}
