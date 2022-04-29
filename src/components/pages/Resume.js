f9f765d0bf917293dc2ea62c176ab176c3df0f9e
Alan Phelps <phelpsa64@gmail.com>
2022-04-29 17:04:09 -0400

    added some user navigation help to the resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,10 +4,11 @@ export default function Resume() {
   return (
     <section id="resume" className="section-alt">
       <article className="content">
+        <h2 className="section-header">Resume</h2>
         <a href="AlanPhelpsResume.pdf" download>
-          <h2 className="section-header">Resume</h2>
+          <h4>Click here to download a copy of my resume.</h4>
         </a>
-        <br/>
+        <br />
         <iframe
           src="https://docs.google.com/document/d/e/2PACX-1vQ4WB6vaijJYwn0uQKuLC2LpdmEd-RUTxUPvVjVOawJKfDhPWpuWHSy36cMvs9iQAP1D5phm3AljZ05/pub?embedded=true"
           className="resume"
