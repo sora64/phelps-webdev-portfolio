d35b34f5e4582cd8e2789ff461b603141cfa49a1
Alan Phelps <phelpsa64@gmail.com>
2022-05-13 16:58:33 -0400

    resume now will download latest version as a PDF; new icons for PWA added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,15 +10,19 @@ export default function Resume() {
   return (
     <footer>
       <h2>Made by Alan Phelps, 2022.</h2>
-      <a href="https://www.linkedin.com/in/alan-phelps-43ab57b6/">
+      <a
+        href="https://www.linkedin.com/in/alan-phelps-43ab57b6/"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FontAwesomeIcon icon={faLinkedin} size="xl" color="white" />
       </a>
       --
-      <a href="https://github.com/sora64">
+      <a href="https://github.com/sora64" target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faGithub} size="xl" color="grey" />
       </a>
       --
-      <a href="https://twitter.com/sora643216">
+      <a href="https://twitter.com/sora643216" target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faTwitter} size="xl" color="#1DA1F2" />
       </a>
     </footer>
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,7 +5,10 @@ export default function Resume() {
     <section id="resume" className="section-alt">
       <article className="content">
         <h2 className="section-header">Resume</h2>
-        <a href="AlanPhelpsResume.pdf" download>
+        <a
+          href="https://docs.google.com/document/d/1CEZw2D5ndJFJqJXz7St1_fmOeJokTfL-jepKGyMavWw/export?format=pdf"
+          download
+        >
           <h4>Click here to download a copy of my resume.</h4>
         </a>
         <br />
