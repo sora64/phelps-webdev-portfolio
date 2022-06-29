f6dc1a63e5e23b0de08f5181f722b0e29fb4ffab
Alan Phelps <phelpsa64@gmail.com>
2022-06-29 13:54:10 -0400

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,14 +6,14 @@ export default function Resume() {
       <article className="content">
         <h2 className="section-header">Resume</h2>
         <a
-          href="https://docs.google.com/document/d/1VMR61Ro0s6gMaS_diw-S6FNiXXtsM7bHyn8IBBH4czs/export?usp=sharing?format=pdf"
+          href="https://docs.google.com/document/d/1VMR61Ro0s6gMaS_diw-S6FNiXXtsM7bHyn8IBBH4czs/export?format=pdf"
           download
         >
           <h4>Click here to download a copy of my resume.</h4>
         </a>
         <br />
         <iframe
-          src="https://docs.google.com/document/d/1VMR61Ro0s6gMaS_diw-S6FNiXXtsM7bHyn8IBBH4czs/pub?usp=sharing?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vRYHu9q2roOyKgeyM9ScGMlN7C-wJA66h282W4c-lZLn-sXJC5pQen-0-gBOcwI6mT-huvk-4oh8p8l/pub?embedded=true"
           className="resume"
           title="myResume"
         ></iframe>
