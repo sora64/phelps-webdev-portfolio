c38c918cb3e88972d2274567dcd0f46cc906af0c
Alan Phelps <phelpsa64@gmail.com>
2022-07-20 21:33:30 -0400

    added beginnings of swiping functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,9 +24,40 @@ export default function Header() {
 
   const handlePageChange = (page) => setCurrentPage(page);
 
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50;
+
+  const onTouchStart = (e) => {
+    setTouchEnd(null); // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX);
+  };
+
+  const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX);
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return;
+    const distance = touchStart - touchEnd;
+    const isLeftSwipe = distance > minSwipeDistance;
+    const isRightSwipe = distance < -minSwipeDistance;
+    if (isLeftSwipe || isRightSwipe)
+      console.log("swipe", isLeftSwipe ? "left" : "right");
+    // add your conditional logic here
+    
+  };
+
   return (
-    <div>
-      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
+    <div
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
       {renderPage()}
       <Footer />
     </div>
