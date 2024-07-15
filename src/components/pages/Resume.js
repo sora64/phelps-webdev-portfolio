d4b4e22e7b42f99abd1111dd973c6da999f96f5f
sora64 <phelpsa64@gmail.com>
2024-07-15 18:14:03 -0400

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,14 +6,14 @@ export default function Resume() {
       <article className="content">
         <h2 className="section-header">Resume</h2>
         <a
-          href="https://docs.google.com/document/d/1VMR61Ro0s6gMaS_diw-S6FNiXXtsM7bHyn8IBBH4czs/export?format=pdf"
+          href="https://docs.google.com/document/d/1OAJ2WdjFO2L1Bn4rYp-_9N7vRfKRYO0VyWZZOHR_Lg4/export?format=pdf"
           download
         >
           <h4>Click here to download a copy of my resume.</h4>
         </a>
         <br />
         <iframe
-          src="https://docs.google.com/document/d/e/2PACX-1vRYHu9q2roOyKgeyM9ScGMlN7C-wJA66h282W4c-lZLn-sXJC5pQen-0-gBOcwI6mT-huvk-4oh8p8l/pub?embedded=true"
+          src="https://docs.google.com/document/d/e/2PACX-1vQ6IYbzQSlE-UqP3-CZcxlM6cHB6wWfFcpBjUnNO1molLfEnE5UYNa1cw_8eKzQIH7IFlX_E4tZrdgz/pub?embedded=true"
           className="resume"
           title="myResume"
         ></iframe>
