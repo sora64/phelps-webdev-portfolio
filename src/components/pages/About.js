0ac3993c797586a14e2c0879f5823dc3417c8cfd
sora64 <phelpsa64@gmail.com>
2024-07-15 16:26:02 -0400

    updated summary with new languages, technologies, and applications

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,16 +14,16 @@ export default function About() {
           <article className="content">
             <h1 className="section-header">About Me</h1>
             <h2 className="subsection-header">Summary</h2>
-            <p>Full Stack Web Developer with a background in Education, Classics, and Linguistics. Efficient, and adept at problem solving to develop user-friendly applications. Known for breadth of knowledge, ability to explain information in understandable ways, and for enjoying challenges.</p>
+            <p>Experienced Full Stack Web Developer with a background in Education, Classics, and Linguistics. Efficient, and adept at problem solving to develop user-friendly applications. Known for breadth of knowledge, ability to explain information in understandable ways, and for enjoying challenges.</p>
             <h2 className="subsection-header">Technical Skills</h2>
             <ul>
               <li>
-                Languages: JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, Python, Java, C#, PHP</li>
+                Programming Languages: JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, Python, Java, PHP, TypeScript</li>
               <li>
-                Applications: GitHub, MongoDB, MySQL, Azure DevOps, Microsoft SQL Server
+                Frameworks and Libraries: React.js, React Native, Express.js, Node.js, Handlebars.js, jQuery, Bootstrap, Bulma, JSP
               </li>
               <li>
-                Tools: Express.js, React.js, Node.js, Handlebars.js, jQuery, Bootstrap, Bulma, JSP
+                Tools and Applications: Git, GitHub, MongoDB, MySQL, MySQL Server, Visual Studio, Visual Studio Code, Android Studio, Figma, Jira, Microsoft Azure DevOps, Microsoft Outlook, Microsoft Teams, Slack, Zoom
               </li>
             </ul>
           </article>
