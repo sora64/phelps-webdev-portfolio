c37cd97e9b4073ca0dcceb0f91d7543f00f2beb1
sora64 <phelpsa64@gmail.com>
2024-07-16 12:26:27 -0400

    separated media queries into their respective stylesheets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/app.css";
-import "./styles/media-queries.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
