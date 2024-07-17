20da328346d675ee377385cf957fb95d38ef9a3a
sora64 <phelpsa64@gmail.com>
2024-07-17 12:12:56 -0400

    changed app.css name to styles.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/app.css";
+import "./styles/styles.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
